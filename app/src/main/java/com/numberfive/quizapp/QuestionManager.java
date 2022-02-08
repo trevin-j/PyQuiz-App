4b92a98f27704ecdd2396c6d5caeadf5c7565874
Trevin Jones <tr3vinj@gmail.com>
2022-02-08 14:43:32 -0700

    Added questions.json; Added assets folder; Created QuestionManager class

---
+++ app/src/main/java/com/numberfive/quizapp/QuestionManager.java
@@ -0,0 +1,27 @@
+package com.numberfive.quizapp;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class QuestionManager {
+
+    /**
+     * qjson holds the json data for all the questions.
+     * It is automatically loaded in when QuestionManager is initialized.
+     */
+    private JSONObject qjson;
+
+    /**
+     * Class constructor
+     */
+    public QuestionManager() {
+
+    }
+
+    /**
+     * Load the questions json into the private qjson field.
+     */
+    private void loadJson() {
+
+    }
+}
