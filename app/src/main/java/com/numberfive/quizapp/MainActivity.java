6388eb78d54d1e7717a2a894546f7f25c9c2090e
mickfish1996 <fis17005@byui.edu>
2022-02-14 14:31:52 -0700

    Added the runActivty function

--- app/src/main/java/com/numberfive/quizapp/MainActivity.java
+++ app/src/main/java/com/numberfive/quizapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String CATAGORY_KEY = "com.numberfive.quizapp.CATAGORY"
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void runActivity(View v) {
+        Intent intent = new Intent(this, question.class);
+        String catagory = onclick();
+        intent.putExtra(CATAGORY_KEY, catagory)
     }
 }
