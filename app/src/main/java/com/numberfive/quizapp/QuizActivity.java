fbeffb400b3fbd677be5b23e12728092b4652743
Trevin Jones <tr3vinj@gmail.com>
2022-02-28 15:11:38 -0700

    Check answer

--- app/src/main/java/com/numberfive/quizapp/QuizActivity.java
+++ app/src/main/java/com/numberfive/quizapp/QuizActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.numberfive.quizapp.questions.Question;
 import com.numberfive.quizapp.questions.QuestionManager;
@@ -12,26 +14,34 @@ import org.json.JSONException;
 
 
 public class QuizActivity extends AppCompatActivity {
-    private QuestionManager questionManager = new QuestionManager();
+    private QuestionManager questionManager;
     Question question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        getQuestion();
     }
 
     private void getQuestion(){
+        questionManager = new QuestionManager(this);
         Intent intent = getIntent();
         String category = intent.getStringExtra(MainActivity.CATEGORY_KEY);
         try{
             question = questionManager.getRandomQuestionByCategory(category);
-        }
-
-        catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
+    public void checkAnswer(View view) {
+        Button button = (Button) view;
+        String selectedAnswer = button.getText().toString();
 
+        boolean correct = question.isCorrect(selectedAnswer);
+        if (correct) {
+
+        }
     }
 }
