bed9a33323d0cfaf72c1e6723e464fe0ef0326f8
mickfish1996 <fis17005@byui.edu>
2022-02-28 14:31:50 -0700

    new calls the question manager and question classes

--- app/src/main/java/com/numberfive/quizapp/QuizActivity.java
+++ app/src/main/java/com/numberfive/quizapp/QuizActivity.java
@@ -2,13 +2,36 @@ package com.numberfive.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.numberfive.quizapp.questions.Question;
+import com.numberfive.quizapp.questions.QuestionManager;
+
+import org.json.JSONException;
+
+
 public class QuizActivity extends AppCompatActivity {
+    private QuestionManager questionManager = new QuestionManager();
+    Question question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    private void getQuestion(){
+        Intent intent = getIntent();
+        String category = intent.getStringExtra(MainActivity.CATEGORY_KEY);
+        try{
+            question = questionManager.getRandomQuestionByCategory(category);
+        }
+
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
